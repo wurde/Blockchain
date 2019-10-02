699f0198f468c21f64105021169fa1e165363763
Andy Bettisworth <andy@accreu.com>
2019-10-02 17:23:33 -0500

    Add a timer to miner.py (stretch)

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -5,28 +5,21 @@
 import hashlib
 import requests
 import json
+import time
 import sys
 
 #
 # Define method to search for proof
 #
 
-def search_for_proof():
+def search_for_proof(last_proof):
     """
     Simple Proof of Work Algorithm
     Find a number p such that hash(last_block_string, p) contains 6 leading
     zeroes
-    :return: A valid proof for the provided block
     """
     
-    proof = 0
-
-    # TODO: Implement functionality to search for a proof 
-    # TODO: Get the last proof from the server and look for a new one
-
-    # block_string = json.dumps(block, sort_keys=True).encode()
-    # while self.valid_proof(block_string, proof) is False:
-    #     proof += 1
+    proof = last_proof + 1
 
     return proof
 
@@ -41,27 +34,33 @@ if __name__ == '__main__':
     else:
         node = "http://localhost:5000"
 
-    res = requests.get(node + '/last-block')
-    res = json.loads(res.content)
-
-    last_block = res['last-block']
-
-    # TODO: (stretch) Add a timer
     coins_mined = 0
     print("Mining has started.")
+    t1_start = time.process_time()
     try:
         while True:
-            new_proof = search_for_proof()
+            # Get the last proof from the server and look for a new one
+            res = requests.get(node + '/last-block')
+            res = json.loads(res.content)
+            last_block = res['last-block']
 
-            # # When found, POST it to the server.
+            proof = search_for_proof(last_block['proof'])
+
+            # block_string = json.dumps(block, sort_keys=True).encode()
+            # while self.valid_proof(block_string, proof) is False:
+            #     proof += 1
+
+            # When found, POST it to the server.
             # res = requests.post(node + '/mine', { "proof": new_proof })
             # res_content = json.loads(res.content)
 
-            # # If the server responds with 'New Block Forged'.
-            # if res.status_code == 200:
+            # If the server responds with 'New Block Forged'.
+            # if res.status_code == 200 and res_content['message'] == 'New Block Forged':
             #     coins_mined += 1
             #     print(f"Total Coins Mined: {coins_mined}")
             # else:
             #     print(res_content['message'])
     except:
         print("Mining has ended.")
+        t1_stop = time.process_time()
+        print("Elapsed time: %.1f seconds" % ((t1_stop-t1_start)))
